92b838eb3fe308f0863cfa0d06122a49e0d57c66
Prat15072000 <56332414+Prat15072000@users.noreply.github.com>
2021-07-12 21:50:11 +0530

    updated the api manager code

--- api_manager.dart
+++ api_manager.dart
@@ -7,7 +7,7 @@ class API_Manager {
   static Future<Transaction> getDetails() async {
     var client = http.Client();
     var transactions;
-
+try{
     var response = await client
         .get(Uri.https("api.skidfintech.com", "accounts/test-data/"));
     print('response' + response.body);
@@ -17,7 +17,15 @@ class API_Manager {
       transactions = Transaction.fromJson(jsonMap);
       print('ok here it is');
     }
-
+    else{
+            print("The response status is not 200");
+            return transactions;
+         }
+}
+catch(e){
+  print("The error is "+e.toString());
+      return transactions;
+}
     return transactions;
   }
 }
